05254b91efea23518b51f12f83951b250a2b0399
Alisa Chan <alisachan.ny@gmail.com>
2020-10-07 12:10:08 -0400

    attempted calling NASA API and displaying NEOs but getting ModuleNotFound for 'detector.urls'

--- neo/detector/views.py
+++ neo/detector/views.py
@@ -1,3 +1,11 @@
+import os
+import json
+import requests
 from django.shortcuts import render
 
 # Create your views here.
+def home():
+  response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key={}'.format(os.getenv('NASA_API_KEY')))
+  data = response.json()
+  print(data)
+  return render("detector/home.html", {"neos": data['near_earth_objects']})
