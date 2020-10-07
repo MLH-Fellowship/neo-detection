83f2477dd3bae6a4bcfe5fa3fc969cc2f7216f44
Ricky Chon <rickychon99@gmail.com>
2020-10-06 23:12:08 -0400

    Update config name, change time zone
    * Update config name from DisplayConfig to DetectorConfig in apps.py
    * Remove detector.apps.DetectorConfig from INSTALLED_APPS, causing
    migration errors
    * Update time zone from UTC to America/New_York

--- neo/settings.py
+++ neo/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'detector.apps.DetectorConfig',
+    # 'detector.apps.DetectorConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/New_York'
 
 USE_I18N = True
 
