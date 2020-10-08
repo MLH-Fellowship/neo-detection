644eb2a91861dc383a00cc1c393e1be346f5370b
Ricky Chon <rickychon99@gmail.com>
2020-10-08 03:49:35 -0400

    Use Django session middleware to set cache expiry

--- neo/detector/views.py
+++ neo/detector/views.py
@@ -1,20 +1,18 @@
 import os
 import json
 import requests
-import time
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     is_cached = ('neodata' in request.session)
     # Refresh cache every 15 minutes once it expires
-    is_expired = ('cache_time' in request.session and time.time() - request.session['cache_time'] >= 60 * 15)
+    request.session.set_expiry(60 * 15)
 
-    if not is_cached or is_expired:
+    if not is_cached:
         params = dict(api_key=os.getenv('NASA_API_KEY'), size=10, page=5)
         response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse', params)
         request.session['neodata'] = response.json()
-        request.session['cache_time'] = time.time()
 
     neodata = request.session['neodata']
 
