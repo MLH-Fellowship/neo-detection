c1f969075f69b87467f095639ec9ef55ebb8481d
chanalisa <52388966+chanalisa@users.noreply.github.com>
2020-10-08 01:28:02 -0400

    Consume API (#9)
    * attempted calling NASA API and displaying NEOs but getting ModuleNotFound for 'detector.urls'
    
    * successfully rendering a list of NEOs from first page of data from API
    
    * cleaned up the html
    
    * updated requirements.txt
    
    * removed duplicate detector directory in root and updated name in apps.py

--- neo/settings.py
+++ neo/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'detector.apps.DetectorConfig',
+    'neo.detector.apps.DetectorConfig',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'neo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
