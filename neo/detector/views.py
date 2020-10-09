9bae15151f2f749be820a0f8c7fd6931cfb2049a
Ricky Chon <rickychon99@gmail.com>
2020-10-09 14:51:16 -0400

    Update API urls, caching for individual pages (#14)
    * If cache does not contain the page, make external API request;
    otherwise get the page from the cache
    * Changed home url to browse
    * Increase session expiry time to 30 min

--- neo/detector/views.py
+++ neo/detector/views.py
@@ -1,24 +1,25 @@
 import os
 import json
 import requests
-from django.shortcuts import render
 from django.http import JsonResponse
 
 # Create your views here.
-def home(request):
-    is_cached = ('neodata' in request.session)
-    # Refresh cache every 15 minutes once it expires
-    request.session.set_expiry(60 * 15)
+def browse(request):
+    if not 'neodata' in request.session:
+        request.session['neodata'] = {}
 
-    if not is_cached:
-        params = dict(api_key=os.getenv('NASA_API_KEY'), size=10, page=5)
+    page = request.GET.get('page', 0)
+
+    # Refresh cache every 30 minutes once it expires
+    request.session.set_expiry(60 * 30)
+
+    if not page in request.session['neodata']:
+        params = dict(api_key=os.getenv('NASA_API_KEY'), size=20, page=page)
         response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse', params)
-        request.session['neodata'] = response.json()
+        request.session['neodata'][page] = response.json()
 
-    neodata = request.session['neodata']
-
-    # return render(request, "detector/home.html", { "neos": neodata['near_earth_objects'], 'is_cached': is_cached })
+    neodata = request.session['neodata'][page]
 
     # Using JsonResponse object (an HTTPResponse subclass) to create a JSON-encoded response. Its default Content-Type header is set to application/json.
     # https://docs.djangoproject.com/en/3.1/ref/request-response/#jsonresponse-objects
-    return JsonResponse({"neos": neodata['near_earth_objects'], 'is_cached': is_cached})
+    return JsonResponse({ "neodata": neodata })
