11081834051791ab502b36a00f5d05f764997bea
chanalisa <52388966+chanalisa@users.noreply.github.com>
2020-10-11 03:51:49 -0400

    added news view (#16)

--- neo/detector/urls.py
+++ neo/detector/urls.py
@@ -1,7 +1,9 @@
   
 from django.urls import path
 from .views import browse
+from .views import news
 
 urlpatterns = [
     path("browse/", browse, name="browse"),
+    path("news/", news, name="news"),
 ]
