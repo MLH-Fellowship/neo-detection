f56dd53093bc339492480d0ec2d46f412f6b0618
Ricky Chon <rickychon99@gmail.com>
2020-10-12 03:41:08 -0400

    Comment out everything for now

--- neo/detector/views.py
+++ neo/detector/views.py
@@ -5,28 +5,28 @@ from django.http import JsonResponse
 
 # Create your views here.
 def browse(request):
-    if not 'neodata' in request.session:
-        request.session['neodata'] = {}
+    # if not 'neodata' in request.session:
+    #     request.session['neodata'] = {}
 
-    page = request.GET.get('page', 0)
+    # page = request.GET.get('page', 0)
 
-    # Refresh cache every 60 minutes once it expires
-    request.session.set_expiry(60 * 60)
+    # # Refresh cache every 60 minutes once it expires
+    # request.session.set_expiry(60 * 60)
 
-    if not page in request.session['neodata']:
-        params = dict(api_key=os.getenv('NASA_API_KEY'), size=20, page=page)
-        response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse', params)
-        request.session['neodata'][page] = response.json()
+    # if not page in request.session['neodata']:
+    #     params = dict(api_key=os.getenv('NASA_API_KEY'), size=20, page=page)
+    #     response = requests.get('https://api.nasa.gov/neo/rest/v1/neo/browse', params)
+    #     request.session['neodata'][page] = response.json()
 
-    neodata = request.session['neodata'][page]
+    # neodata = request.session['neodata'][page]
     
-    if 'links' in neodata:
-        del neodata['links']
+    # if 'links' in neodata:
+    #     del neodata['links']
     
-    if 'near_earth_objects' in neodata:
-        for obj in neodata['near_earth_objects']:
-            if 'links' in obj:
-                del obj['links']
+    # if 'near_earth_objects' in neodata:
+    #     for obj in neodata['near_earth_objects']:
+    #         if 'links' in obj:
+    #             del obj['links']
 
     # Using JsonResponse object (an HTTPResponse subclass) to create a JSON-encoded response. Its default Content-Type header is set to application/json.
     # https://docs.djangoproject.com/en/3.1/ref/request-response/#jsonresponse-objects
