3aaeb767893941b268d0309df24a7053a744de1d
Ricky Chon <rickychon99@gmail.com>
2020-10-12 02:12:56 -0400

    Set static root

--- neo/settings.py
+++ neo/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/'
 
 # Whitelist localhost:3000 because that's where the frontend will be served
 CORS_ORIGIN_WHITELIST = [
