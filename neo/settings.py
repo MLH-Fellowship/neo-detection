f797fcec702ede4ff17c6737bdb398be76c89cbb
Ricky Chon <rickychon99@gmail.com>
2020-10-12 03:26:03 -0400

    Update allowed hosts

--- neo/settings.py
+++ neo/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'neo-detection.herokuapp.com',
-]
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
