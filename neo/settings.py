23ec31c08d7ff3d80e1862b8efd4fb997a047feb
Ricky Chon <rickychon99@gmail.com>
2020-10-12 07:04:38 -0400

    Cors origin allow all

--- neo/settings.py
+++ neo/settings.py
@@ -19,6 +19,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+CORS_ORIGIN_ALLOW_ALL = True # added to solve CORS
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
